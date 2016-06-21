7549a000c7179aafb53ac0960eda98446f5a75cc
rosapergams <rosa@pergams.com>
2016-06-21 13:46:52 -0400

    Increase load time by eliminating render-blocking JavaScript and CSS in above-the-fold content & optimizing images

--- javascript/main.js
+++ javascript/main.js
@@ -1,8 +1,8 @@
 (function($) {
 	$(document).ready(function() {
-		$(window).load(function(){
+		// $(window).load(function(){
 			$('.loaded').css({opacity: 0.0, visibility: "visible"}).animate({opacity: 1.0},'slow');
 		});
 		$.slidebars();
-	});
+	// });
 }) (jQuery);
