9fd583d69541137787c544052d446e602e2a96d6
rosapergams <rosa@pergams.com>
2016-06-21 19:48:18 -0400

    Add links to projects & add 'Mouth' project

--- javascript/main.js
+++ javascript/main.js
@@ -1,6 +1,6 @@
 (function($) {
 	$(document).ready(function() {
-		$( ".parallax__layer--back" ).load( "ajax/test.html", function() {
+		$(".parallax__layer--back").load( "ajax/test.html", function() {
 			$('.loaded').css({opacity: 0.0, visibility: "visible"}).animate({opacity: 1.0},'slow');
 		});
 		$.slidebars();
