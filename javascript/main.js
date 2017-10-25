4cef47678a86a2a00a3b33cbc5bcc3c407a6911a
rosapergams <rosa@pergams.com>
2017-10-25 14:47:46 -0500

    Replace contents w/ new website design && assets

--- javascript/main.js
+++
@@ -1,8 +0,0 @@
-(function($) {
-	$(document).ready(function() {
-		$(".parallax__layer--back").load( "ajax/test.html", function() {
-			$('.loaded').css({opacity: 0.0, visibility: "visible"}).animate({opacity: 1.0},'slow');
-		});
-		$.slidebars();
-	});
-}) (jQuery);
